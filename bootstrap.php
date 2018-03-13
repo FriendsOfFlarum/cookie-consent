f9817dae7002ab17c28279bf6e1eb3316ab59a64
Joel Bond <joel.bond@zaptech.ca>
2018-03-13 17:15:05 -0600

    added loadsettingsfromdatabase listener

--- bootstrap.php
+++ bootstrap.php
@@ -5,4 +5,5 @@ use Illuminate\Contracts\Events\Dispatcher;
 
 return function (Dispatcher $events) {
     $events->subscribe(Listeners\AddClientAssets::class);
+    $events->subscribe(Listeners\LoadSettingsFromDatabase::class);
 };
