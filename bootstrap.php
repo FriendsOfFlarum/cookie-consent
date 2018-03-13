3dfbc77461450519962f21486aebbaa36376e6bf
Joel Bond <joel.bond@zaptech.ca>
2018-03-13 16:10:39 -0600

    removed unnecessary line of code

--- bootstrap.php
+++ bootstrap.php
@@ -5,5 +5,4 @@ use Illuminate\Contracts\Events\Dispatcher;
 
 return function (Dispatcher $events) {
     $events->subscribe(Listeners\AddClientAssets::class);
-    //$events->subscribe(Listeners\LoadSettingsFromDatabase::class);
 };
