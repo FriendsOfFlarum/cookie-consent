e09d79053ba2fe02b3c626ece60e86463a321eb7
Joel Bond <joel.bond@zaptech.ca>
2018-03-13 17:24:23 -0600

    added beta code

--- js/admin/src/main.js
+++ js/admin/src/main.js
@@ -7,6 +7,25 @@ import Page from 'flarum/components/Page';
 app.initializers.add('zaptech-cookie-consent', () => {
   app.extensionSettings['zaptech-cookie-consent'] = () => app.modal.show(new CookieConsentSettingsModal());
       extend(Page.prototype, 'init', function() {
-        document.querySelector("head").innerHTML += '<style>#app{background:' + adminpagehex + '}</style>';
+        document.querySelector("head").innerHTML += '<link rel="stylesheet" type="text/css" href="//cdnjs.cloudflare.com/ajax/libs/cookieconsent2/3.0.3/cookieconsent.min.css" />';
+        document.querySelector("head").innerHTML += '<script src="//cdnjs.cloudflare.com/ajax/libs/cookieconsent2/3.0.3/cookieconsent.min.js"></script>';
+        document.querySelector("head").innerHTML += '    <script>';
+        document.querySelector("head").innerHTML += 'window.addEventListener("load", function(){';
+        document.querySelector("head").innerHTML += 'window.cookieconsent.initialise({';
+        document.querySelector("head").innerHTML += '  "palette": {';
+        document.querySelector("head").innerHTML += '    "popup": {';
+        document.querySelector("head").innerHTML += '      "background": "#000"';
+        document.querySelector("head").innerHTML += '    },';
+        document.querySelector("head").innerHTML += '    "button": {';
+        document.querySelector("head").innerHTML += '      "background": "#f1d600"';
+        document.querySelector("head").innerHTML += '    }';
+        document.querySelector("head").innerHTML += '  },';
+        document.querySelector("head").innerHTML += '  "content": {';
+        document.querySelector("head").innerHTML += '    "message": "This website",';
+        document.querySelector("head").innerHTML += '    "dismiss": "got",';
+        document.querySelector("head").innerHTML += '    "link": "me"';
+        document.querySelector("head").innerHTML += '  }';
+        document.querySelector("head").innerHTML += '})});';
+        document.querySelector("head").innerHTML += '</script>';
       });
   });
