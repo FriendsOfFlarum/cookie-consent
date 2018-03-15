489326acc0f45e7b550e0a497689c30ffbc5ea68
Joel Bond <joel.bond@zaptech.ca>
2018-03-15 17:17:33 -0600

    changed from #extGlobalNav to #cookieconsent

--- js/forum/src/main.js
+++ js/forum/src/main.js
@@ -8,9 +8,9 @@ app.initializers.add('zaptech-cookie-consent', () => {
     extend(Page.prototype, 'init', function() {
         var text;
         text = app.forum.attribute("cookie-consent.ctext");
-        $('#header').prepend('<div id="extGlobalNav"></div>');
-        $('#extGlobalNav').append('<link rel="stylesheet" type="text/css" href="//cdnjs.cloudflare.com/ajax/libs/cookieconsent2/3.0.3/cookieconsent.min.css" /> <script src="//cdnjs.cloudflare.com/ajax/libs/cookieconsent2/3.0.3/cookieconsent.min.js"></script> <script>window.addEventListener("load", function(){window.cookieconsent.initialise({ "palette": { "popup": { "background": "#000" }, "button": { "background": "#f1d600" } }, "content": { "message": "This website", "dismiss": "got", "link": "me" }})});</script>');
-        const toppadding = Number($('#extGlobalNav').height()) + 52;
+        $('#header').prepend('<div id="cookieconsent"></div>');
+        $('#cookieconsent').append('<link rel="stylesheet" type="text/css" href="//cdnjs.cloudflare.com/ajax/libs/cookieconsent2/3.0.3/cookieconsent.min.css" /> <script src="//cdnjs.cloudflare.com/ajax/libs/cookieconsent2/3.0.3/cookieconsent.min.js"></script> <script>window.addEventListener("load", function(){window.cookieconsent.initialise({ "palette": { "popup": { "background": "#000" }, "button": { "background": "#f1d600" } }, "content": { "message": "This website", "dismiss": "got", "link": "me" }})});</script>');
+        const toppadding = Number($('#cookieconsent').height()) + 52;
     ;
     });
 });
