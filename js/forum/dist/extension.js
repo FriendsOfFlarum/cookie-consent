b322cd914c42c91f6d5ace5f7eb674e4262721c8
Joel Bond <joel.bond@zaptech.ca>
2018-03-22 16:46:34 -0600

    added atext var to the dismiss button text on the forum-side

--- js/forum/dist/extension.js
+++ js/forum/dist/extension.js
@@ -22,8 +22,9 @@ System.register('zaptech/cookie-consent/main', ['flarum/app', 'flarum/extend', '
                     ctext = app.forum.attribute("cookie-consent.ctext");
                     btext = app.forum.attribute("cookie-consent.btext");
                     blink = app.forum.attribute("cookie-consent.blink");
+                    atext = app.forum.attribute("cookie-consent.atext");
                     $('head').prepend('<div id="cookieconsent"></div>');
-                    $('#cookieconsent').append('<link rel="stylesheet" type="text/css" href="//cdnjs.cloudflare.com/ajax/libs/cookieconsent2/3.0.3/cookieconsent.min.css"/><script src="//cdnjs.cloudflare.com/ajax/libs/cookieconsent2/3.0.3/cookieconsent.min.js"></script><script>window.addEventListener("load", function(){window.cookieconsent.initialise({"palette":{"popup":{"background": "#a62424", "text": "#000000"}, "button":{"background": "#735151", "text": "#4d2d2d"}}, "theme": "classic", "content":{"message": "' + ctext + '", "dismiss": "Alright?", "link": "' + btext + '", "href": "' + blink + '"}})});</script>');
+                    $('#cookieconsent').append('<link rel="stylesheet" type="text/css" href="//cdnjs.cloudflare.com/ajax/libs/cookieconsent2/3.0.3/cookieconsent.min.css"/><script src="//cdnjs.cloudflare.com/ajax/libs/cookieconsent2/3.0.3/cookieconsent.min.js"></script><script>window.addEventListener("load", function(){window.cookieconsent.initialise({"palette":{"popup":{"background": "#a62424", "text": "#000000"}, "button":{"background": "#735151", "text": "#4d2d2d"}}, "theme": "classic", "content":{"message": "' + ctext + '", "dismiss": "' + atext + '", "link": "' + btext + '", "href": "' + blink + '"}})});</script>');
                     var toppadding = Number($('#cookieconsent').height()) + 52;
                     ;
                 });
