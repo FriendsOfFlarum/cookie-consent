08610df817627924f524b067975a134b1681b117
Joel Bond <joel.bond@zaptech.ca>
2018-03-22 20:46:41 -0600

    removed bcolor1 from forum main.js

--- js/forum/src/main.js
+++ js/forum/src/main.js
@@ -15,7 +15,7 @@ app.initializers.add('zaptech-cookie-consent', () => {
         bcolor1 = app.forum.attribute("cookie-consent.bcolor1");
         bcolor2 = app.forum.attribute("cookie-consent.bcolor2");
         $('head').prepend('<div id="cookieconsent"></div>');        
-        $('#cookieconsent').append('<link rel="stylesheet" type="text/css" href="//cdnjs.cloudflare.com/ajax/libs/cookieconsent2/3.0.3/cookieconsent.min.css"/><script src="//cdnjs.cloudflare.com/ajax/libs/cookieconsent2/3.0.3/cookieconsent.min.js"></script><script>window.addEventListener("load", function(){window.cookieconsent.initialise({"palette":{"popup":{"background": "' + bcolor + '", "text": "' + bcolor1 + '"}, "button":{"background": "' + bcolor2 + '", "text": "' + bcolor1 + '"}}, "theme": "classic", "content":{"message": "' + ctext + '", "dismiss": "' + atext + '", "link": "' + btext + '", "href": "' + blink + '"}})});</script>');
+        $('#cookieconsent').append('<link rel="stylesheet" type="text/css" href="//cdnjs.cloudflare.com/ajax/libs/cookieconsent2/3.0.3/cookieconsent.min.css"/><script src="//cdnjs.cloudflare.com/ajax/libs/cookieconsent2/3.0.3/cookieconsent.min.js"></script><script>window.addEventListener("load", function(){window.cookieconsent.initialise({"palette":{"popup":{"background": "' + bcolor + '", "text": "#000000"}, "button":{"background": "' + bcolor2 + '", "text": "#000000"}}, "theme": "classic", "content":{"message": "' + ctext + '", "dismiss": "' + atext + '", "link": "' + btext + '", "href": "' + blink + '"}})});</script>');
         const toppadding = Number($('#cookieconsent').height()) + 52;
     });
 });
