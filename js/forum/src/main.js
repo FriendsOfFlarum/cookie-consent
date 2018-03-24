b3afdd0e3f3d6fa38ab61a03bce45174eb3bc7c7
Joel Bond <joel.bond@zaptech.ca>
2018-03-23 21:33:56 -0600

    changed forum sdide to lmtext too

--- js/forum/src/main.js
+++ js/forum/src/main.js
@@ -6,16 +6,16 @@ import Page from 'flarum/components/Page';
 
 app.initializers.add('zaptech-cookie-consent', () => {
     extend(Page.prototype, 'init', function() {
-        var ctext, btext, blink, atext, bcolor, bcolor1, bcolor2;
+        var ctext, btext, blink, lmtext, bcolor, bcolor1, bcolor2;
         ctext = app.forum.attribute("cookie-consent.ctext");
         btext = app.forum.attribute("cookie-consent.btext");
         blink = app.forum.attribute("cookie-consent.blink");
-        atext = app.forum.attribute("cookie-consent.atext");
+        lmtext = app.forum.attribute("cookie-consent.lmtext");
         bcolor = app.forum.attribute("cookie-consent.bcolor");
         bcolor1 = app.forum.attribute("cookie-consent.bcolor1");
         bcolor2 = app.forum.attribute("cookie-consent.bcolor2");
         $('head').prepend('<div id="cookieconsent"></div>');        
-        $('#cookieconsent').append('<link rel="stylesheet" type="text/css" href="//cdnjs.cloudflare.com/ajax/libs/cookieconsent2/3.0.3/cookieconsent.min.css"/><script src="//cdnjs.cloudflare.com/ajax/libs/cookieconsent2/3.0.3/cookieconsent.min.js"></script><script>window.addEventListener("load", function(){window.cookieconsent.initialise({"palette":{"popup":{"background": "' + bcolor + '", "text": "#000000"}, "button":{"background": "' + bcolor2 + '", "text": "#000000"}}, "theme": "classic", "content":{"message": "' + ctext + '", "dismiss": "' + atext + '", "link": "' + btext + '", "href": "' + blink + '"}})});</script>');
+        $('#cookieconsent').append('<link rel="stylesheet" type="text/css" href="//cdnjs.cloudflare.com/ajax/libs/cookieconsent2/3.0.3/cookieconsent.min.css"/><script src="//cdnjs.cloudflare.com/ajax/libs/cookieconsent2/3.0.3/cookieconsent.min.js"></script><script>window.addEventListener("load", function(){window.cookieconsent.initialise({"palette":{"popup":{"background": "' + bcolor + '", "text": "#000000"}, "button":{"background": "' + bcolor2 + '", "text": "#000000"}}, "theme": "classic", "content":{"message": "' + ctext + '", "dismiss": "' + lmtext + '", "link": "' + btext + '", "href": "' + blink + '"}})});</script>');
         const toppadding = Number($('#cookieconsent').height()) + 52;
     });
 });
