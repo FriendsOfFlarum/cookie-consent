50fa0d33b956c8e3ae0e99ea4a7c2142c6c83428
Joel Bond <joel.bond@zaptech.ca>
2018-03-26 13:47:19 -0600

    added if statement, and changed bcolor1, 2, 3 to fbcolor

--- js/forum/src/main.js
+++ js/forum/src/main.js
@@ -6,7 +6,7 @@ import Page from 'flarum/components/Page';
 
 app.initializers.add('zaptech-cookie-consent', () => {
     extend(Page.prototype, 'init', function() {
-        var ctext, btext, blink, lrntext, bcolor, bcolor1, bcolor2;
+        var ctext, btext, blink, lrntext, bcolor, bcolor1, bcolor2, bcolor3, fbcolor;
         ctext = app.forum.attribute("cookie-consent.ctext");
         btext = app.forum.attribute("cookie-consent.btext");
         blink = app.forum.attribute("cookie-consent.blink");
@@ -14,7 +14,13 @@ app.initializers.add('zaptech-cookie-consent', () => {
         bcolor = app.forum.attribute("cookie-consent.bcolor");
         bcolor1 = app.forum.attribute("cookie-consent.bcolor1");
         bcolor2 = app.forum.attribute("cookie-consent.bcolor2");
-        $('head').prepend('<link rel="stylesheet" type="text/css" href="//cdnjs.cloudflare.com/ajax/libs/cookieconsent2/3.0.3/cookieconsent.min.css"/><script src="//cdnjs.cloudflare.com/ajax/libs/cookieconsent2/3.0.3/cookieconsent.min.js"></script><script>window.addEventListener("load", function(){window.cookieconsent.initialise({"palette":{"popup":{"background": "' + bcolor + '"}, "button":{"background": "' + bcolor2 + '"}}, "theme": "classic", "content":{"message": "' + ctext + '", "dismiss": "' + btext + '", "link": "' + lrntext + '", "href": "' + blink + '"}})});</script>');
+        bcolor3 = app.forum.attribute("cookie-consent.bcolor3");
+        if (bcolor3 = 0) {
+            fbcolor = '#000000';
+          } else if (bcolor3 = 1) {
+            fbcolor = '#ffffff';
+          };
+        $('head').prepend('<link rel="stylesheet" type="text/css" href="//cdnjs.cloudflare.com/ajax/libs/cookieconsent2/3.0.3/cookieconsent.min.css"/><script src="//cdnjs.cloudflare.com/ajax/libs/cookieconsent2/3.0.3/cookieconsent.min.js"></script><script>window.addEventListener("load", function(){window.cookieconsent.initialise({"palette":{"popup":{"background": "' + fbcolor + '"}, "button":{"background": "' + fbcolor + '"}}, "theme": "classic", "content":{"message": "' + ctext + '", "dismiss": "' + btext + '", "link": "' + lrntext + '", "href": "' + blink + '"}})});</script>');
         const toppadding = Number($('#cookieconsent').height()) + 52;
     });
 });
