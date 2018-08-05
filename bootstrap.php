3e241cf70c0c6e40c9459fef4cc8d3f36ea16e48
Joel Bond <joel.bond@partialdev.com>
2018-08-04 19:29:40 -0600

    Update bootstrap.php for beta8

--- bootstrap.php
+++ bootstrap.php
@@ -1,12 +1,24 @@
 <?php
-namespace ZapTech\CConsent;
-
-use Illuminate\Contracts\Events\Dispatcher;
 
 /**
- * @param Dispatcher $events
+ *  This file is part of reflar/enso-theme.
+ *
+ *  Copyright (c) ReFlar.
+ *
+ *  http://reflar.io
+ *
+ *  For the full copyright and license information, please view the license.md
+ *  file that was distributed with this source code.
  */
-return function (Dispatcher $events) {
-    $events->subscribe(Listeners\AddClientAssets::class);
-    $events->subscribe(Listeners\LoadSettingsFromDatabase::class);
-};
+
+namespace Zaptech\CConsent;
+use Flarum\Extend;
+use Illuminate\Contracts\Events\Dispatcher;
+return [
+    (new Extend\Frontend('forum'))
+        ->js(__DIR__.'/js/dist/forum.js')
+        ->css(__DIR__.'/resources/less/forum.less'),
+    (new Extend\Frontend('admin'))
+        ->js(__DIR__.'/js/dist/admin.js')
+        ->css(__DIR__.'/resources/less/admin.less')
+];
