808b04114e622f09ca555d60d2712a0ae7b2990a
Joel Bond <joel.bond@partialdev.com>
2018-08-05 14:46:32 -0600

    More updates

--- bootstrap.php
+++ bootstrap.php
@@ -23,6 +23,7 @@ return [
     (new Extend\Frontend('admin'))
         ->js(__DIR__.'/js/dist/admin.js')
         ->css(__DIR__.'/resources/less/admin.less'),
+    new Extend\Locales(__DIR__.'/resources/locale'),
     function (Dispatcher $events) {
         $events->subscribe(Listeners\LoadSettingsFromDatabase::class);
     },
