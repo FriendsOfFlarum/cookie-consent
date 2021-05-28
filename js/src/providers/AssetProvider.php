e96431fbc73b7c07493219ce49fa4f5b84cc5e21
Katos <7927609+katosdev@users.noreply.github.com>
2021-05-28 21:16:09 +0100

    Remove function from Extend
    Move function to its own provider

---
+++ js/src/providers/AssetProvider.php
@@ -0,0 +1,31 @@
+<?php
+
+/*
+ * This file is part of fof/cookie-consent.
+ *
+ * Copyright (c) FriendsOfFlarum.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace FoF\CookieConsent\Providers;
+
+use Flarum\Formatter\Formatter;
+use Flarum\Foundation\AbstractServiceProvider;
+use Flarum\Foundation\Paths;
+use Flarum\Frontend\Assets;
+
+class PageServiceProvider extends AbstractServiceProvider
+{
+    public function boot()
+    {
+        $container->resolving('flarum.assets.forum', function (Assets $assets) {
+            if (resolve['flarum.settings']->get('reflar-cookie-consent.ccTheme') != 'no_css') {
+                $assets->css(function (SourceCollector $sources) {
+                    $sources->addFile(__DIR__.'/resources/less/forum.less');
+                });
+            }
+        });
+    }
+}
