938065bda37d9f0607b691149010454f950acdf8
Katos <7927609+katosdev@users.noreply.github.com>
2021-05-28 21:32:58 +0100

    Update extend.php
    It's all about the commas!

--- extend.php
+++ extend.php
@@ -31,7 +31,7 @@ return [
     new Extend\Locales(__DIR__.'/resources/locale'),
 
     (new Extend\ServiceProvider())
-        ->register(Providers\AssetProvider::class)
+        ->register(Providers\AssetProvider::class),
 
     (new Extend\ApiSerializer(ForumSerializer::class))
         ->attributes(Listeners\LoadSettingsFromDatabase::class),
