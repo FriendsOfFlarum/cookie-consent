0308b929cee39a56bce6cdedb5dcb1f8fa5c3918
Ian Morland <ian@morland.me>
2021-09-09 11:47:30 +0100

    Remove fof/components dep

--- js/src/admin/components/CookieConsentSettingsPage.js
+++ js/src/admin/components/CookieConsentSettingsPage.js
@@ -1,10 +1,5 @@
+import app from 'flarum/admin/app';
 import ExtensionPage from 'flarum/admin/components/ExtensionPage';
-
-import { settings } from '@fof-components';
-
-const {
-    items: { StringItem, SelectItem },
-} = settings;
 
 export default class CookieConsentSettingsModal extends ExtensionPage {
     oninit(vnode) {
@@ -17,67 +12,81 @@ export default class CookieConsentSettingsModal extends ExtensionPage {
         return [
             <div className="container">
                 <div className="CookieConsentSettingsPage">
-                    <div className="Form-group">
-                        <h2>{app.translator.trans('fof-cookie-consent.admin.settings.configuration_title')}</h2>
+                    <div className="Form">
                         <div className="Form-group">
-                            <label>{app.translator.trans('fof-cookie-consent.admin.settings.consentText')}</label>
-                            <textarea required className="FormControl" bidi={this.setting('fof-cookie-consent.consentText')} />
-                        </div>
-                        <StringItem name="fof-cookie-consent.buttonText" setting={this.setting.bind(this)}>
-                            {app.translator.trans('fof-cookie-consent.admin.settings.buttonText')}
-                        </StringItem>
-
-                        <h3>{app.translator.trans('fof-cookie-consent.admin.settings.configuration_button_title')}</h3>
-                        <div className="Form-section">
-                            <StringItem name="fof-cookie-consent.learnMoreLinkText" setting={this.setting.bind(this)} required>
-                                {app.translator.trans('fof-cookie-consent.admin.settings.learnMoreLinkText')}
-                            </StringItem>
-                            <StringItem name="fof-cookie-consent.learnMoreLinkUrl" setting={this.setting.bind(this)}>
-                                {app.translator.trans('fof-cookie-consent.admin.settings.learnMoreLinkUrl')}
-                            </StringItem>
-                        </div>
-
-                        <h2>{app.translator.trans('fof-cookie-consent.admin.settings.theme_title')}</h2>
-
-                        <div className="Form-group">
-                            <label>{app.translator.trans('fof-cookie-consent.admin.settings.ccTheme')}</label>
-                            {SelectItem.component({
-                                options: {
-                                    blocky: app.translator.trans('fof-cookie-consent.admin.settings.themes.blocky'),
-                                    edgeless: app.translator.trans('fof-cookie-consent.admin.settings.themes.edgeless'),
-                                    classic: app.translator.trans('fof-cookie-consent.admin.settings.themes.classic'),
-                                    custom: app.translator.trans('fof-cookie-consent.admin.settings.themes.custom'),
-                                    no_css: app.translator.trans('fof-cookie-consent.admin.settings.themes.no_css'),
-                                },
-                                value: this.setting('fof-cookie-consent.ccTheme')(),
-                                onchange: this.setting('fof-cookie-consent.ccTheme'),
-                                setting: this.setting.bind(this),
-                                required: true,
+                            <h2>{app.translator.trans('fof-cookie-consent.admin.settings.configuration_title')}</h2>
+                            <div className="Form-group">
+                                <label>{app.translator.trans('fof-cookie-consent.admin.settings.consentText')}</label>
+                                <textarea required className="FormControl" bidi={this.setting('fof-cookie-consent.consentText')} />
+                            </div>
+                            {this.buildSettingComponent({
+                                type: 'string',
+                                setting: 'fof-cookie-consent.buttonText',
+                                label: app.translator.trans('fof-cookie-consent.admin.settings.buttonText'),
                             })}
-                        </div>
+                            <h3>{app.translator.trans('fof-cookie-consent.admin.settings.configuration_button_title')}</h3>
+                            <div className="Form-section">
+                                {this.buildSettingComponent({
+                                    type: 'string',
+                                    setting: 'fof-cookie-consent.learnMoreLinkText',
+                                    label: app.translator.trans('fof-cookie-consent.admin.settings.learnMoreLinkText'),
+                                })}
+                                {this.buildSettingComponent({
+                                    type: 'string',
+                                    setting: 'fof-cookie-consent.learnMoreLinkUrl',
+                                    label: app.translator.trans('fof-cookie-consent.admin.settings.learnMoreLinkUrl'),
+                                })}
+                            </div>
 
-                        <h3>{app.translator.trans('fof-cookie-consent.admin.settings.theme_popup_title')}</h3>
+                            <h2>{app.translator.trans('fof-cookie-consent.admin.settings.theme_title')}</h2>
 
-                        <div className="Form-section">
-                            <StringItem name="fof-cookie-consent.backgroundColor" setting={this.setting.bind(this)}>
-                                {app.translator.trans('fof-cookie-consent.admin.settings.backgroundColor')}
-                            </StringItem>
-                            <StringItem name="fof-cookie-consent.textColor" setting={this.setting.bind(this)}>
-                                {app.translator.trans('fof-cookie-consent.admin.settings.textColor')}
-                            </StringItem>
-                        </div>
+                            <div className="Form-group">
+                                {this.buildSettingComponent({
+                                    type: 'select',
+                                    setting: 'fof-cookie-consent.ccTheme',
+                                    label: app.translator.trans('fof-cookie-consent.admin.settings.ccTheme'),
+                                    options: {
+                                        blocky: app.translator.trans('fof-cookie-consent.admin.settings.themes.blocky'),
+                                        edgeless: app.translator.trans('fof-cookie-consent.admin.settings.themes.edgeless'),
+                                        classic: app.translator.trans('fof-cookie-consent.admin.settings.themes.classic'),
+                                        custom: app.translator.trans('fof-cookie-consent.admin.settings.themes.custom'),
+                                        no_css: app.translator.trans('fof-cookie-consent.admin.settings.themes.no_css'),
+                                    },
+                                    required: true,
+                                })}
+                            </div>
 
-                        <h3>{app.translator.trans('fof-cookie-consent.admin.settings.theme_dismiss_title')}</h3>
-                        <div className="Form-section">
-                            <StringItem name="fof-cookie-consent.buttonBackgroundColor" setting={this.setting.bind(this)}>
-                                {app.translator.trans('fof-cookie-consent.admin.settings.buttonBackgroundColor')}
-                            </StringItem>
-                            <StringItem name="fof-cookie-consent.buttonTextColor" setting={this.setting.bind(this)}>
-                                {app.translator.trans('fof-cookie-consent.admin.settings.buttonTextColor')}
-                            </StringItem>
+                            <h3>{app.translator.trans('fof-cookie-consent.admin.settings.theme_popup_title')}</h3>
+
+                            <div className="Form-section">
+                                {this.buildSettingComponent({
+                                    type: 'string',
+                                    setting: 'fof-cookie-consent.backgroundColor',
+                                    label: app.translator.trans('fof-cookie-consent.admin.settings.backgroundColor'),
+                                })}
+                                {this.buildSettingComponent({
+                                    type: 'string',
+                                    setting: 'fof-cookie-consent.textColor',
+                                    label: app.translator.trans('fof-cookie-consent.admin.settings.textColor'),
+                                })}
+                            </div>
+
+                            <h3>{app.translator.trans('fof-cookie-consent.admin.settings.theme_dismiss_title')}</h3>
+                            <div className="Form-section">
+                                {this.buildSettingComponent({
+                                    type: 'string',
+                                    setting: 'fof-cookie-consent.buttonBackgroundColor',
+                                    label: app.translator.trans('fof-cookie-consent.admin.settings.buttonBackgroundColor'),
+                                })}
+                                {this.buildSettingComponent({
+                                    type: 'string',
+                                    setting: 'fof-cookie-consent.buttonTextColor',
+                                    label: app.translator.trans('fof-cookie-consent.admin.settings.buttonTextColor'),
+                                })}
+                            </div>
                         </div>
+                        {this.submitButton()}
                     </div>
-                    {this.submitButton()}
                 </div>
             </div>,
         ];
